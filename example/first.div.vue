a0f54a00c43eae8ed56e1c17c85975a1127fe812
Alperen Şahin <allperensahin@gmail.com>
2021-09-22 14:11:05 +0300

    Add generate HTML

---
+++ example/first.div.vue
@@ -0,0 +1,16 @@
+
+        <template>
+            <div>
+                <div class="Allah">Buyuk</div><div class=""></div> 
+            </div> 
+        </template>
+        <script>
+         
+        </script>
+        <style scoped>
+        #{id}{
+          background-color: antiquewhite;
+          color: antiquewhite;
+        }
+        </style>
+        
